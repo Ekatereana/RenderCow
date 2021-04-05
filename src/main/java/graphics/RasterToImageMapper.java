5cbcdcb97ba116dc2ba8786bbd32f2fe870dac4c
Ekatereana <ekatereanagricaenko@gmail.com>
2021-04-05 22:20:21 +0300

    add basic raster

---
+++ src/main/java/graphics/RasterToImageMapper.java
@@ -0,0 +1,20 @@
+package graphics;
+
+import java.awt.*;
+
+public interface RasterToImageMapper {
+
+    Image mapToImage();
+
+    void setPixel(int x, int y, int rgb);
+    void setPixel(int x, int y, byte red, byte green, byte blue);
+
+    Color getPixelColor(int x, int y);
+    int getPixelRGB(int x, int y);
+
+    int getWidth();
+    int getHeight();
+
+
+
+}
---
+++ src/main/java/scenes/AbstractScene.java
@@ -0,0 +1,9 @@
+package scenes;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public abstract class AbstractScene {
+}
